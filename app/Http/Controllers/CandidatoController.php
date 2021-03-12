8395f0c239e26617b6012789d706d1d088c28141
Gabriel-31415 <gabriel.uag.ufrpe@gmail.com>
2021-03-12 16:35:38 +0000

    function uploaud file

--- app/Http/Controllers/CandidatoController.php
+++ app/Http/Controllers/CandidatoController.php
@@ -6,5 +6,15 @@ use Illuminate\Http\Request;
 
 class CandidatoController extends Controller
 {
-    //
+
+    
+
+    public function uploadFile($request, $input, $nome){
+    	if($request->hasFile($input)){
+    		$path = $request->photo->storeAs('images', $nome, 'public');
+
+    		return $path;
+    	}
+    	return null;
+    }
 }
