7b0e4f9e32386741f01ce7d06fdae17ec116c23a
Gabriel-31415 <gabriel.uag.ufrpe@gmail.com>
2021-03-18 18:49:01 +0000

    distribuir incompleto

--- resources/views/livewire/distribuir.blade.php
+++ resources/views/livewire/distribuir.blade.php
@@ -2,25 +2,26 @@
     <div class="py-12">
         <div class="max-w-3xl mx-auto sm:px-6 lg:px-8">
             <div class="container">
-                <form action="{{ route('lotes.store') }}" method="post">
+                <form  wire:submit.prevent="calcular">
                     @csrf
                     <div class="row">
                         <div class="col-md-12">
-                            <h4>Nº de vacinas do lote: {{ $lote->qtdVacina }}</h4>
-                            {{-- <label for="nome"></label> --}}
+                            <h4>Nº de vacinas do lote: {{ intdiv ( $lote->qtdVacina , $postos->count() ) . ' vacinas/posto' }}</h4>
+                            {{-- <input type="text" wire:model="$lote->qtdVacina"> --}}
+                            @foreach ($array_vacinas_disponiveis as $key => $item)
+                                {{ $key }}
+                                {{ $item  }}
+                            @endforeach
 
                         </div>
 
                     </div>
                     <div class="row">
-                        @foreach ($postos as $posto)
+                        @foreach ($postos as $key => $posto)
                             <div class="col-md-12">
                                 <div class="input-group mb-3">
-                                    <div class="input-group-prepend">
-                                      <button class="btn btn-info" type="button">Confirmar</button>
-                                    </div>
                                     <div class="custom-file">
-                                        <input type="text" class="form-control" value="{{ $posto->vacinas_disponiveis }}">
+                                        <input type="number" class="form-control" wire:model="array_vacinas_disponiveis.{{$posto->nome}}" >
                                         <div class="input-group-append">
                                             <span class="input-group-text" id="basic-addon2">{{ $posto->nome }}</span>
                                           </div>
@@ -35,7 +36,7 @@
                     <br>
                     <div class="row">
                         <div class="col-md-3">
-                            <button class="btn btn-success">Salvar</button>
+                            <button type="submit" class="btn btn-success">Salvar</button>
                         </div>
                     </div>
                 </form>
