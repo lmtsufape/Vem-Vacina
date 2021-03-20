5718680eba248d9746715a3fa7bd3059aafb0bab
Gabriel-31415 <gabriel.uag.ufrpe@gmail.com>
2021-03-20 03:26:23 +0000

    Update database connection

--- config/database.php
+++ config/database.php
@@ -1,13 +1,7 @@
 <?php
 
 use Illuminate\Support\Str;
-$DATABASE_URL=parse_url('postgres://uypoywcucarvch:6af0b4e6fefd310a863312c0d9aeeae1b295cfc62de902c66d5e4215d4b6066c@ec2-52-7-115-250.compute-1.amazonaws.com:5432/d2g24fgjf9mg45');
-$url = parse_url(getenv("DATABASE_URL"));
-
-$host = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$database = substr($url["path"], 1);
+$DATABASE_URL=parse_url('postgres://aomzkcpejlmhjr:bc62f55cbdfbf3a6ac6eb1bb1db6d45e54d1118907742951b88fce429bf2ba27@ec2-52-7-115-250.compute-1.amazonaws.com:5432/ddpqso67hap6hd');
 return [
 
     /*
@@ -71,13 +65,16 @@ return [
 
         'pgsql' => array(
             'driver'   => 'pgsql',
-            'host'     => $host,
-            'database' => $database,
-            'username' => $username,
-            'password' => $password,
+            'host'     => $DATABASE_URL['host'],
+            'port'     => $DATABASE_URL['port'],
+            'database' => ltrim($DATABASE_URL['path'], "/"),
+            'username' => $DATABASE_URL['user'],
+            'password' => $DATABASE_URL['pass'],
             'charset'  => 'utf8',
             'prefix'   => '',
+            'prefix_indexes' => true,
             'schema'   => 'public',
+            'sslmode'   => 'prefer',
         ),
 
         'sqlsrv' => [
