d4aa0401b9f29416eaea5662db3626d8881a08e9
Carlos André <carlos.andre1270@gmail.com>
2021-03-27 22:27:16 -0300

    crase

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -31,7 +31,7 @@
                                                             @if ($i != 0) <hr> @endif
                                                             {{$etapa->inicio_intervalo}}
                                                             <span class="style_card_apresentacao_a_anos"> 
-                                                                à 
+                                                                a 
                                                             </span>{{$etapa->fim_intervalo}}
                                                             <span class="style_card_apresentacao_a_anos"> 
                                                                 anos 
@@ -87,7 +87,7 @@
                                             <div class="row">
                                                 <div class="col-md-4">
                                                     <div class="row style_card_divisao_horizontal" >
-                                                        <div class="col-md-12 style_card_medio_conteudo">{{$etapa->inicio_intervalo}} à {{$etapa->fim_intervalo}} anos</div>
+                                                        <div class="col-md-12 style_card_medio_conteudo">{{$etapa->inicio_intervalo}} a {{$etapa->fim_intervalo}} anos</div>
                                                         <div class="col-md-12 style_card_medio_legenda">FAIXA ETÁRIA</div>
                                                     </div>
                                                 </div>
