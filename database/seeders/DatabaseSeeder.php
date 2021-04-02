137d1b91588be6e147804a4d4e03262edf06666e
Carlos André <carlos.andre1270@gmail.com>
2021-04-01 21:57:07 -0300

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,44 +19,44 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
         $this->call([
             // LoteSeeder::class,
-            // PostoVacinacaoSeeder::class,
-            // EtapaSeeder::class,
-            // OpcoesEtapaSeeder::class,
-            // OutrasInfoEtapaSeeder::class,
-            // ConfiguracaoSeeder::class,
-            CandidatoSeeder::class,
+            PostoVacinacaoSeeder::class,
+            EtapaSeeder::class,
+            OpcoesEtapaSeeder::class,
+            OutrasInfoEtapaSeeder::class,
+            ConfiguracaoSeeder::class,
+            // CandidatoSeeder::class,
         ]);
 
-        // DB::table('users')->insert([
-        //     'name' => 'Admin',
-        //     'email' => 'gabriel.uag.ufrpe@gmail.com',
-        //     'tipo' => User::TIPO_ENUM['admin'],
-        //     'password' => Hash::make('12345678'),
-        //     'email_verified_at' => now(),
-        // ]);
+        DB::table('users')->insert([
+            'name' => 'Admin',
+            'email' => 'gabriel.uag.ufrpe@gmail.com',
+            'tipo' => User::TIPO_ENUM['admin'],
+            'password' => Hash::make('12345678'),
+            'email_verified_at' => now(),
+        ]);
 
-        // DB::table('users')->insert([
-        //     'name' => 'gerente',
-        //     'email' => 'gerente@admin.com',
-        //     'tipo' => User::TIPO_ENUM['gerente'],
-        //     'password' => Hash::make('12345678'),
-        //     'email_verified_at' => now(),
-        // ]);
+        DB::table('users')->insert([
+            'name' => 'gerente',
+            'email' => 'gerente@admin.com',
+            'tipo' => User::TIPO_ENUM['gerente'],
+            'password' => Hash::make('12345678'),
+            'email_verified_at' => now(),
+        ]);
 
-        // DB::table('users')->insert([
-        //     'name' => 'colaborador',
-        //     'email' => 'colaborador@admin.com',
-        //     'tipo' => User::TIPO_ENUM['colaborador'],
-        //     'password' => Hash::make('12345678'),
-        //     'email_verified_at' => now(),
-        // ]);
+        DB::table('users')->insert([
+            'name' => 'colaborador',
+            'email' => 'colaborador@admin.com',
+            'tipo' => User::TIPO_ENUM['colaborador'],
+            'password' => Hash::make('12345678'),
+            'email_verified_at' => now(),
+        ]);
 
-        // DB::table('users')->insert([
-        //     'name' => 'secretaria',
-        //     'email' => 'secretaria@admin.com',
-        //     'tipo' => User::TIPO_ENUM['secretaria'],
-        //     'password' => Hash::make('12345678'),
-        //     'email_verified_at' => now(),
-        // ]);
+        DB::table('users')->insert([
+            'name' => 'secretaria',
+            'email' => 'secretaria@admin.com',
+            'tipo' => User::TIPO_ENUM['secretaria'],
+            'password' => Hash::make('12345678'),
+            'email_verified_at' => now(),
+        ]);
     }
 }
