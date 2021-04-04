0f93532ce83305d24fe1624b8d62a4612f7696d1
Gabriel-31415 <gabriel.uag.ufrpe@gmail.com>
2021-04-04 01:18:34 +0000

    ajustes

--- app/Http/Controllers/ImportController.php
+++ app/Http/Controllers/ImportController.php
@@ -16,9 +16,21 @@ class ImportController extends Controller
     public function store(Request $request)
     {
         $file = $request->file('file');
+        try {
+            Excel::import(new CandidatoImport, $file);
+        } catch (\Maatwebsite\Excel\Validators\ValidationException $e) {
+             $failures = $e->failures();
 
-        Excel::import(new CandidatoImport, $file);
+             foreach ($failures as $failure) {
+                 $failure->row(); // row that went wrong
+                 $failure->attribute(); // either heading key (if using heading row concern) or column index
+                 $failure->errors(); // Actual error messages from Laravel validator
+                 $failure->values(); // The values of the row that has failed.
+             }
+        }
 
-        return back()->with(['status' => 'Solicitação realizada com sucesso!']);
+
+
+        return back()->with(['mensagem' => 'Solicitação realizada com sucesso!']);
     }
 }
--- app/Imports/CandidatoImport.php
+++ app/Imports/CandidatoImport.php
@@ -2,17 +2,24 @@
 
 namespace App\Imports;
 
-use App\Candidato;
-use App\Models\Etapa;
-use Maatwebsite\Excel\Concerns\ToModel;
-use Carbon\Carbon;
-use Maatwebsite\Excel\Concerns\SkipsOnError;
-use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Exception;
 use Throwable;
+use App\Models\Candidato;
+use Carbon\Carbon;
+use App\Models\Etapa;
+use App\Http\Controllers\Controller;
+use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\Importable;
+use Maatwebsite\Excel\Concerns\SkipsOnError;
+use Maatwebsite\Excel\Concerns\SkipsOnFailure;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Maatwebsite\Excel\Concerns\WithValidation;
+use Maatwebsite\Excel\Imports\HeadingRowFormatter;
 
-class CandidatoImport implements ToModel, SkipsOnError, WithHeadingRow
+class CandidatoImport implements ToModel, WithHeadingRow, SkipsOnError
 {
 
+    use Importable;
     /**
     * @param array $row
     *
@@ -21,54 +28,58 @@ class CandidatoImport implements ToModel, SkipsOnError, WithHeadingRow
     public function model(array $row)
     {
 
+        $controller = new Controller();
+        // dd($row);
+        $idade = $this->idade($row['data_de_nascimento']);
 
-
-        $idade = $this->idade($row['Data de nascimento']);
-
-        if ($row[1] == 'Idosos de 70 a 74 anos') {
-            $etapa = Etapa::find(4);
-        }elseif ($row[1] == 'Idosos de 65 a 69 anos') {
-            $etapa = Etapa::find(5);;
+        if ($row['grupo_prioritario'] == 'Idosos de 70 a 74 anos') {
+            $etapa = Etapa::find(1); //4
+        }elseif ($row['grupo_prioritario'] == 'Idosos de 65 a 69 anos') {
+            $etapa = Etapa::find(2); //5
+        }else{
+            return null;
         }
 
-        if (!$this->validar_telefone($row[8])) {
-            throw new Exception('error');
+        if (!$this->validar_telefone($row['telefone_para_contato'])) {
+            return null;
         }
 
-        if (!$this->validar_cpf($row[4])) {
-            throw new Exception('error');
+        if (!$this->validar_cpf($row['informe_seu_cpf'])) {
+            return null;
         }
 
         if ($etapa->tipo == Etapa::TIPO_ENUM[0]) {
             if (!($etapa->inicio_intervalo <= $idade && $etapa->fim_intervalo >= $idade)) {
-                throw new Exception('error');
+                return null;
             }
         }
 
         $candidato = new Candidato([
-            'nome_completo' => $row['Nome completo'],
-            'data_de_nascimento' =>$row['Data de nascimento'],
-            'cpf' => $row['Informe seu CPF'],
-            'numero_cartao_sus' => $row['Número do cartão do SUS'],
-            'sexo' => $row['Sexo'],
+            'nome_completo' => $row['nome_completo'],
+            'data_de_nascimento' =>$row['data_de_nascimento'],
+            'cpf' => $row['informe_seu_cpf'],
+            'numero_cartao_sus' => $row['numero_do_cartao_do_sus'],
+            'sexo' => $row['sexo'],
             'idade' => $idade,
-            'nome_da_mae' => $row['Nome completo da mãe'],
-            'telefone' => $row['Telefone para contato'],
-            'whatsapp' => $row['WhatsApp'],
-            'email' => $row['E-mail'],
-            'cep' => $row['CEP'] == "" ? NULL : $row['CEP'],
+            'nome_da_mae' => $row['nome_completo_da_mae'],
+            'telefone' => $row['telefone_para_contato'],
+            'whatsapp' => $row['whatsapp'],
+            'email' => $row['e_mail'],
+            'cep' => $row['cep'] == "" ? NULL : $row['cep'],
             'cidade' => "Garanhuns",
-            'bairro' => $row['Bairro'],
-            'logradouro' => $row['Nome da rua'],
-            'numero_residencia' => $row['Número da casa'],
+            'bairro' => $row['bairro'],
+            'logradouro' => $row['nome_da_rua'],
+            'numero_residencia' => $row['numero_da_casa'],
             'complemento_endereco' => " ",
             'aprovacao' => Candidato::APROVACAO_ENUM[1],
             'dose' => Candidato::DOSE_ENUM[0],
             'etapa_id' => $etapa->id,
 
         ]);
-        if (  $row['O idoso é acamado?'] == 'Sim' ) {
-            $candidato->outrasInfo()->attach(1);
+        if ($etapa->outrasInfo != null && count($etapa->outrasInfo) > 0) {
+            if (  $row['o_idoso_e_acamado'] == 'Sim' ) {
+                $candidato->outrasInfo()->attach(1);
+            }
         }
         return $candidato;
     }
@@ -79,9 +90,44 @@ class CandidatoImport implements ToModel, SkipsOnError, WithHeadingRow
         return $hoje->diffInYears($data_nascimento);
     }
 
-    public function onError(Throwable $e)
+    public function onError(\Throwable $e)
+    {
+        // Handle the exception how you'd like.
+    }
+
+    protected function validar_telefone($telefone) {
+        return preg_match('/^\(\d{2}\)\s?\d{5}-\d{4}$/', $telefone) > 0;
+    }
+
+    protected function validar_cpf($cpf)
     {
 
+        // https://gist.github.com/rafael-neri/ab3e58803a08cb4def059fce4e3c0e40
+
+        // Extrai somente os números
+        $cpf = preg_replace('/[^0-9]/is', '', $cpf);
+
+        // Verifica se foi informado todos os digitos corretamente
+        if (strlen($cpf) != 11) {
+            return false;
+        }
+
+        // Verifica se foi informada uma sequência de digitos repetidos. Ex: 111.111.111-11
+        if (preg_match('/(\d)\1{10}/', $cpf)) {
+            return false;
+        }
+
+        // Faz o calculo para validar o CPF
+        for ($t = 9; $t < 11; $t++) {
+            for ($d = 0, $c = 0; $c < $t; $c++) {
+                $d += $cpf[$c] * (($t + 1) - $c);
+            }
+            $d = ((10 * $d) % 11) % 10;
+            if ($cpf[$c] != $d) {
+                return false;
+            }
+        }
+        return true;
     }
 
 
