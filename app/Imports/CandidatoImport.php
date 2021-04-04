ff99b3962d4aaf73f18dc48920ed3fcb544b1c12
Gabriel-31415 <gabriel.uag.ufrpe@gmail.com>
2021-04-04 02:41:58 +0000

    ajustes

--- app/Imports/CandidatoImport.php
+++ app/Imports/CandidatoImport.php
@@ -29,7 +29,10 @@ class CandidatoImport implements ToModel, WithHeadingRow, SkipsOnError
     {
 
         $controller = new Controller();
-        dd($row);
+        // dd($row);
+        if (!isset($row['data_de_nascimento'])) {
+            return null;
+        }
         $idade = $this->idade($row['data_de_nascimento']);
         $row['informe_seu_cpf'] = $this->formatar_cpf_cnpj($row['informe_seu_cpf']);
         if ($row['grupo_prioritario'] == 'Idosos de 70 a 74 anos') {
