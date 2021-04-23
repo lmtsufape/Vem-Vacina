77a37d29c960ad8ec825e1d76b2aa890eb91874a
Carlos André <carlos.andre1270@gmail.com>
2021-04-23 13:16:16 -0300

    deve ser um array no importar lista

--- app/Http/Controllers/ImportController.php
+++ app/Http/Controllers/ImportController.php
@@ -38,45 +38,47 @@ class ImportController extends Controller
 
         $hoje = Carbon::today();
         foreach($line_of_text as $i => $line) {
-            $existe = Candidato::where('cpf', $line[4])->first();
-            if ($i != 0 && $existe == null) {
-                $candidato = new Candidato;
-                
-                $candidato->nome_completo = $line[2];
-                $candidato->data_de_nascimento = new Carbon($line[3]);
-                $candidato->cpf     = $line[4];
-                $candidato->numero_cartao_sus = $line[5];
-                $candidato->sexo = $line[6];
-                $candidato->nome_da_mae = $line[7];
-                $candidato->telefone = $line[8];
-                $candidato->whatsapp = $line[9];
-                $candidato->email = $line[10];
-                $candidato->logradouro = $line[11];
-                $candidato->numero_residencia = $line[12];
-                $candidato->bairro = $line[13];
-                $candidato->cep    = $line[14]; 
-                $candidato->aprovacao = Candidato::APROVACAO_ENUM[0];
-                $candidato->dose = Candidato::DOSE_ENUM[0];
-                $candidato->cidade = "Garanhuns";
-                $idade = $hoje->diffInYears(new Carbon($line[3]));
-                $candidato->idade = $idade;
+            if (is_array($line)) {
+                $existe = Candidato::where('cpf', $line[4])->first();
+                if ($i != 0 && $existe == null) {
+                    $candidato = new Candidato;
+                    
+                    $candidato->nome_completo = $line[2];
+                    $candidato->data_de_nascimento = new Carbon($line[3]);
+                    $candidato->cpf     = $line[4];
+                    $candidato->numero_cartao_sus = $line[5];
+                    $candidato->sexo = $line[6];
+                    $candidato->nome_da_mae = $line[7];
+                    $candidato->telefone = $line[8];
+                    $candidato->whatsapp = $line[9];
+                    $candidato->email = $line[10];
+                    $candidato->logradouro = $line[11];
+                    $candidato->numero_residencia = $line[12];
+                    $candidato->bairro = $line[13];
+                    $candidato->cep    = $line[14]; 
+                    $candidato->aprovacao = Candidato::APROVACAO_ENUM[0];
+                    $candidato->dose = Candidato::DOSE_ENUM[0];
+                    $candidato->cidade = "Garanhuns";
+                    $idade = $hoje->diffInYears(new Carbon($line[3]));
+                    $candidato->idade = $idade;
 
-                $etapa = Etapa::where([['tipo', Etapa::TIPO_ENUM[0]], ['inicio_intervalo', '<=', $idade], ['fim_intervalo', '>=', $idade]])->first();
-                
-                if ($etapa == null) {
-                    return redirect()->back()->withErrors([
-                        "agendamentos" => $line[2] . " com CPF " . $line[4] . " não se encaixa em nenhum público cadastrado."
-                    ])->withInput();
-                }
-                $candidato->etapa_id = $etapa->id;
-                $candidato->save();
-
-                if ($line[15] == "Sim") {
-                    if ($etapa->outrasInfo != null && count($etapa->outrasInfo) > 0) {
-                        $candidato->outrasInfo()->attach($etapa->outrasInfo[0]->id);
+                    $etapa = Etapa::where([['tipo', Etapa::TIPO_ENUM[0]], ['inicio_intervalo', '<=', $idade], ['fim_intervalo', '>=', $idade]])->first();
+                    
+                    if ($etapa == null) {
+                        return redirect()->back()->withErrors([
+                            "agendamentos" => $line[2] . " com CPF " . $line[4] . " não se encaixa em nenhum público cadastrado."
+                        ])->withInput();
                     }
+                    $candidato->etapa_id = $etapa->id;
+                    $candidato->save();
+
+                    if ($line[15] == "Sim") {
+                        if ($etapa->outrasInfo != null && count($etapa->outrasInfo) > 0) {
+                            $candidato->outrasInfo()->attach($etapa->outrasInfo[0]->id);
+                        }
+                    }
+                    $candidato->update();
                 }
-                $candidato->update();
             }
         }
         // try {
