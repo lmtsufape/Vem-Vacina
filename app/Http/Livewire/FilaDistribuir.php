4b542e09a6b6bbc2ab44367494948fe6608f925e
Gabriel-31415 <gabriel.uag.ufrpe@gmail.com>
2021-04-23 20:08:21 +0000

    ajuste

--- app/Http/Livewire/FilaDistribuir.php
+++ app/Http/Livewire/FilaDistribuir.php
@@ -109,9 +109,10 @@ class FilaDistribuir extends Component
                 if ($candidatos_no_mesmo_horario_no_mesmo_lugar->count() > 0) {
                     continue;
                 }
-                if (Candidato::where('cpf',$candidato->cpf)->where('aprovacao',Candidato::APROVACAO_ENUM[0])->orWhere('aprovacao', Candidato::APROVACAO_ENUM[1])->count() > 0) {
+                if (Candidato::where('cpf',$candidato->cpf)->count() > 0) {
                     continue 2;
                 }
+
                 $etapa = $candidato->etapa;
 
                 if(!$etapa->lotes->count()){
