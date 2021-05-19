42330dc31f793213dd4ed6247de08132d560eec1
Gabriel-31415 <gabriel.uag.ufrpe@gmail.com>
2021-05-19 21:30:14 +0000

    ajuste

--- routes/web.php
+++ routes/web.php
@@ -87,7 +87,7 @@ Route::middleware(['auth'])->group(function () {
     Route::get('exportar/postos/candidato', [ExportController::class, 'exportPostoCandidato'])->name('export.exportPostoCandidato');
     Route::get('exportar/index', [ExportController::class, 'exportPostoCandidato'])->name('export.index');
     Route::get('exportar/listaCandidato', [ExportController::class, 'listarCandidato'])->name('export.candidatos');
-    Route::get('exportar/gerar', [ExportController::class, 'gerar'])->name('export.gerar');
+    Route::post('exportar/gerar', [ExportController::class, 'gerar'])->name('export.gerar');
     Route::get('/exportar/agendamentos/posto/{id}', [ExportController::class,'agendamentosDoPosto'])->name('export.agendamentos.posto');
     Route::get('/configuracoes', [ConfiguracaoController::class, 'index'])->name('config.index');
     Route::get('/configuracoes/salvar', [ConfiguracaoController::class, 'update'])->name('config.update');
