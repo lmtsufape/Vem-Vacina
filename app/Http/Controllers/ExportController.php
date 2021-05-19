e72a1093e1c56b365d320a753d9c3d2f67bfa604
Gabriel-31415 <gabriel.uag.ufrpe@gmail.com>
2021-05-19 10:40:24 +0000

    ajuste

--- app/Http/Controllers/ExportController.php
+++ app/Http/Controllers/ExportController.php
@@ -157,7 +157,7 @@ class ExportController extends Controller
     {
         // dd( array_column(json_decode($request->candidatos), 'id')  );
         $ids = array_column(json_decode($request->candidatos), 'id');
-        $candidatos = Candidato::whereIn('id', $ids)->get();
+        $candidatos = Candidato::whereIn('id', $ids)->take(400)->get();
         return Excel::download(new PostoCandidatoExport( $candidatos), 'postosCandidato.xlsx');
     }
     public function listarCandidato()
