7db60181d2a6f79ef8358505202384bbbae5be90
Gabriel-31415 <gabriel.uag.ufrpe@gmail.com>
2021-07-15 03:58:38 +0000

    ajuste

--- resources/views/postos/index_novo.blade.php
+++ resources/views/postos/index_novo.blade.php
@@ -62,6 +62,7 @@
                               <th scope="col">#</th>
                               <th scope="col">Nome</th>
                               <th scope="col">Endereço</th>
+                              <th scope="col">Dias</th>
                               <th scope="col" colspan="2">Ações</th>
                           </tr>
                       </thead>
@@ -70,7 +71,49 @@
                               <tr  data-toggle="collapse" data-target="#demo{{ $posto->id }}" >
                                   <td><i class="fas fa-angle-down  fa-2x"></i> </td>
                                   <td> {{ $posto->nome }}</td>
-                                  <td>{{ $posto->endereco }}</td>
+                                  <td>
+                                    <span class="d-inline-block text-truncate" class="d-inline-block" tabindex="0" data-toggle="tooltip" title="{{ $posto->endereco }}" style="max-width: 150px;">
+                                        {{ $posto->endereco  }}
+                                    </span>
+
+                                  </td>
+                                  <td scope="row">
+                                        @if($posto->funciona_domingo)
+                                            <small class="text-success">D</small>
+                                        @else
+                                            <small class="text-danger">D</small>
+                                        @endif
+                                        @if($posto->funciona_segunda)
+                                            <small class="text-success">S</small>
+                                        @else
+                                            <small class="text-danger">S</small>
+                                        @endif
+                                        @if($posto->funciona_terca)
+                                            <small class="text-success">T</small>
+                                        @else
+                                            <small class="text-danger">T</small>
+                                        @endif
+                                        @if($posto->funciona_quarta)
+                                            <small class="text-success">Q</small>
+                                        @else
+                                            <small class="text-danger">Q</small>
+                                        @endif
+                                        @if($posto->funciona_quinta)
+                                            <small class="text-success">Q</small>
+                                        @else
+                                            <small class="text-danger">Q</small>
+                                        @endif
+                                        @if($posto->funciona_sexta)
+                                            <small class="text-success">S</small>
+                                        @else
+                                            <small class="text-danger">S</small>
+                                        @endif
+                                        @if($posto->funciona_sabado)
+                                            <small class="text-success">S</small>
+                                        @else
+                                            <small class="text-danger">S</small>
+                                        @endif
+                                  </td>
                                   <td>
                                       @can('editar-posto')
                                           <form action="{{ route('postos.edit', ['posto' => $posto->id]) }}" method="get">
@@ -95,75 +138,77 @@
                               </tr>
                               <tr id="demo{{ $posto->id }}" class="collapse @if($posto->id == old('posto_id') ) show @endif">
 
-                                      <td colspan="5" class="hiddenRow">
-                                          <table class="table table-bordered table-info">
-                                            <thead>
-                                              <tr>
-                                                <th scope="col">Nº do lote</th>
-                                                <th scope="col">Faixa</th>
-                                                <th scope="col">Fabricante</th>
-                                                <th scope="col">Dose única</th>
-                                                <th scope="col">Tempo para a segunda dose</th>
-                                                <th scope="col">Nº de vacinas <i class="fas fa-exclamation-circle"  data-toggle="tooltip" data-placement="top" title="Quantidade de vacinas - quantidade de candidatos nesse lote = vacinas disponíveis"></i></th>
-                                                <th scope="col" colspan="2">Ações</th>
-                                              </tr>
-                                            </thead>
-                                            <tbody>
-                                                @php
-                                                    $pivot = $lotes_pivot->where('posto_vacinacao_id', $posto->id);
-                                                    $candidatosPosto = $posto->candidatos();
-                                                @endphp
-                                              @foreach ($pivot as $key => $lote_pivot)
-                                              <tr>
-                                                  <th scope="row">{{$lote_pivot->lote->numero_lote}}</th>
-                                                  <td>
-                                                  @foreach ($lote_pivot->lote->etapas as $key1 => $etapa)
+                                      <td colspan="6" class="hiddenRow">
+                                        <div class="table-responsive">
+                                            <table class="table table-bordered table-info">
+                                              <thead>
+                                                <tr>
+                                                  <th scope="col">Nº do lote</th>
+                                                  <th scope="col">Faixa</th>
+                                                  <th scope="col">Fabricante</th>
+                                                  <th scope="col">Dose única</th>
+                                                  <th scope="col">Tempo para a segunda dose</th>
+                                                  <th scope="col">Nº de vacinas <i class="fas fa-exclamation-circle"  data-toggle="tooltip" data-placement="top" title="Quantidade de vacinas - quantidade de candidatos nesse lote = vacinas disponíveis"></i></th>
+                                                  <th scope="col" colspan="2">Ações</th>
+                                                </tr>
+                                              </thead>
+                                              <tbody>
+                                                  @php
+                                                      $pivot = $lotes_pivot->where('posto_vacinacao_id', $posto->id);
+                                                      $candidatosPosto = $posto->candidatos();
+                                                  @endphp
+                                                @foreach ($pivot as $key => $lote_pivot)
+                                                <tr>
+                                                    <th scope="row">{{$lote_pivot->lote->numero_lote}}</th>
+                                                    <td>
+                                                    @foreach ($lote_pivot->lote->etapas as $key1 => $etapa)
 
-                                                  @if ($etapa->tipo == $tipos[0])
-                                                    <span class="d-inline-block text-truncate" class="d-inline-block" tabindex="0" data-toggle="tooltip" title="{{ $lote_pivot->lote->etapas->count() > 1 ? 'De '.$etapa->inicio_intervalo." às ".$etapa->fim_intervalo ."/" : 'De '.$etapa->inicio_intervalo." às ".$etapa->fim_intervalo  }}" style="max-width: 150px;">
-                                                        {{ $lote_pivot->lote->etapas->count() > 1 ? 'De '.$etapa->inicio_intervalo." às ".$etapa->fim_intervalo ."/" : 'De '.$etapa->inicio_intervalo." às ".$etapa->fim_intervalo  }}
-                                                    </span>
+                                                    @if ($etapa->tipo == $tipos[0])
+                                                      <span class="d-inline-block text-truncate" class="d-inline-block" tabindex="0" data-toggle="tooltip" title="{{ $lote_pivot->lote->etapas->count() > 1 ? 'De '.$etapa->inicio_intervalo." às ".$etapa->fim_intervalo ."/" : 'De '.$etapa->inicio_intervalo." às ".$etapa->fim_intervalo  }}" style="max-width: 150px;">
+                                                          {{ $lote_pivot->lote->etapas->count() > 1 ? 'De '.$etapa->inicio_intervalo." às ".$etapa->fim_intervalo ."/" : 'De '.$etapa->inicio_intervalo." às ".$etapa->fim_intervalo  }}
+                                                      </span>
 
-                                                  @elseif($etapa->tipo == $tipos[1] || $etapa->tipo == $tipos[2])
-                                                    <span class="d-inline-block text-truncate" class="d-inline-block" tabindex="0" data-toggle="tooltip" title="{{$etapa->texto."-"}}" style="max-width: 150px;">
-                                                        {{$etapa->texto."-"}}
-                                                    </span>
-                                                  @endif
-                                                  @endforeach
-                                                </td>
-                                                <th scope="row">
-                                                    <span class="d-inline-block text-truncate" class="d-inline-block" tabindex="0" data-toggle="tooltip" title="{{$lote_pivot->lote->fabricante}}" style="max-width: 100px;">
-                                                        {{$lote_pivot->lote->fabricante}}
-                                                    </span>
-                                                </th>
-                                                <td>{{$lote_pivot->lote->dose_unica ? 'Sim' : 'Não'}}</td>
-                                                <td>{{$lote_pivot->lote->dose_unica ? " - " : 'Entre '.$lote_pivot->lote->inicio_periodo." à  ". $lote_pivot->lote->fim_periodo." dias" }} </td>
-                                                <td>{{($lote_pivot->qtdVacina - $posto->candidatos()->where('lote_id', $lote_pivot->id)->count())}}</td>
-                                                {{-- <td>{{$lote_pivot->qtdVacina}}</td> --}}
-                                                {{-- <td>{{($posto->candidatos()->where('lote_id', $lote_pivot->id)->count())}}</td> --}}
-                                                {{-- <td>{{($lote_pivot->candidatos()->count() )}}</td> --}}
+                                                    @elseif($etapa->tipo == $tipos[1] || $etapa->tipo == $tipos[2])
+                                                      <span class="d-inline-block text-truncate" class="d-inline-block" tabindex="0" data-toggle="tooltip" title="{{$etapa->texto."-"}}" style="max-width: 150px;">
+                                                          {{$etapa->texto."-"}}
+                                                      </span>
+                                                    @endif
+                                                    @endforeach
+                                                  </td>
+                                                  <th scope="row">
+                                                      <span class="d-inline-block text-truncate" class="d-inline-block" tabindex="0" data-toggle="tooltip" title="{{$lote_pivot->lote->fabricante}}" style="max-width: 100px;">
+                                                          {{$lote_pivot->lote->fabricante}}
+                                                      </span>
+                                                  </th>
+                                                  <td>{{$lote_pivot->lote->dose_unica ? 'Sim' : 'Não'}}</td>
+                                                  <td>{{$lote_pivot->lote->dose_unica ? " - " : 'Entre '.$lote_pivot->lote->inicio_periodo." à  ". $lote_pivot->lote->fim_periodo." dias" }} </td>
+                                                  <td>{{($lote_pivot->qtdVacina - $posto->candidatos()->where('lote_id', $lote_pivot->id)->count())}}</td>
+                                                  {{-- <td>{{$lote_pivot->qtdVacina}}</td> --}}
+                                                  {{-- <td>{{($posto->candidatos()->where('lote_id', $lote_pivot->id)->count())}}</td> --}}
+                                                  {{-- <td>{{($lote_pivot->candidatos()->count() )}}</td> --}}
 
-                                                <td scope="row">
-                                                  <form action="{{ route('lotes.alterarQuantidadeVacina') }}" method="post">
-                                                      @csrf
-                                                      <input type="hidden" name="posto_id" value="{{ $posto->id }}">
-                                                      <input type="hidden" name="lote_id" value="{{ $lote_pivot->id }}">
-                                                      <input type="hidden" name="lote_original_id" value="{{ $lote_pivot->lote->id }}">
-                                                      <div class="row">
-                                                          <div class="col-6">
-                                                              <input class="form-control" name="quantidade"  min="1" type="number" placeholder="Quantidade">
-                                                          </div>
-                                                          <div class="col-2">
-                                                              <button class="btn btn-success">Devolver</button>
-                                                          </div>
-                                                      </div>
-                                                  </form>
-                                                </td>
-                                              </tr>
+                                                  <td scope="row">
+                                                    <form action="{{ route('lotes.alterarQuantidadeVacina') }}" method="post">
+                                                        @csrf
+                                                        <input type="hidden" name="posto_id" value="{{ $posto->id }}">
+                                                        <input type="hidden" name="lote_id" value="{{ $lote_pivot->id }}">
+                                                        <input type="hidden" name="lote_original_id" value="{{ $lote_pivot->lote->id }}">
+                                                        <div class="row">
+                                                            <div class="col-6">
+                                                                <input class="form-control" name="quantidade"  min="1" type="number" placeholder="Quantidade">
+                                                            </div>
+                                                            <div class="col-2">
+                                                                <button class="btn btn-success">Devolver</button>
+                                                            </div>
+                                                        </div>
+                                                    </form>
+                                                  </td>
+                                                </tr>
 
-                                              @endforeach
-                                            </tbody>
-                                          </table>
+                                                @endforeach
+                                              </tbody>
+                                            </table>
+                                        </div>
                                       </td>
                               </tr>
                           @endforeach
