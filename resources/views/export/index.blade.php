023de35518b7a511b72ee6caceab0cb572376d47
Gabriel-31415 <gabriel.uag.ufrpe@gmail.com>
2021-07-16 22:52:56 +0000

    mais velhos exportar

--- resources/views/export/index.blade.php
+++ resources/views/export/index.blade.php
@@ -78,7 +78,7 @@
                                     </div>
                                     <div class="col-md-3">
                                         <input type="checkbox" name="posicao_check" id="posicao_check" @if($request->posicao_check != null && $request->posicao_check) checked @endif>
-                                        <label>Mais velhos</label>
+                                        <label>Posição</label>
                                     </div>
                                 </div>
                                 <div class="row">
