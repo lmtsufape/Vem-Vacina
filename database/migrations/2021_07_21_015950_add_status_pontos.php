2238c2e7b2208c0d095fa43f86c6ac1969c412e2
Gabriel-31415 <gabriel.uag.ufrpe@gmail.com>
2021-07-21 05:02:38 +0000

    add status em ponto

---
+++ database/migrations/2021_07_21_015950_add_status_pontos.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddStatusPontos extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posto_vacinacaos', function (Blueprint $table) {
+            $table->string('status') // Nome da coluna
+                    ->nullable() // Preenchimento não obrigatório
+                    ->after('padrao_no_formulario');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posto_vacinacaos', function (Blueprint $table) {
+            $table->dropColumn('status');
+        });
+    }
+}
