baa67a4a4d28034e2837ee3a2f6c612d5f284556
Gabriel-31415 <gabriel.uag.ufrpe@gmail.com>
2021-08-15 02:26:03 +0000

    ajuste me exportar

--- resources/views/export/candidatos.blade.php
+++ resources/views/export/candidatos.blade.php
@@ -5,35 +5,34 @@ use App\Models\LotePostoVacinacao;
 <table>
     <thead>
     <tr>
-        <th>#</th>
-        <th>Nome</th>
-        <th>data_de_nascimento</th>
+        <th>Grupo Prioritário</th>
+        <th>Local de Trabalho</th>
+        <th>Função</th>
+        <th>CPF do Vacinado</th>
+        <th>CNS do Vacinado</th>
+        <th>Nome do Vacinado</th>
+        <th>Data de Nacimento</th>
+        <th>Sexo</th>
+        <th>Nome da Mãe</th>
+        <th>Data de Vacinação</th>
+        <th>Nome da Vacina</th>
+        <th>Dose</th>
+        <th>Lote</th>
+        <th>Produtor</th>
+        <th>Nome da vacinador (a)</th>
+        <th>Digitado por</th>
         <th>Idade</th>
-        <th>cpf</th>
-        <th>numero_cartao_sus</th>
-        <th>sexo</th>
-        <th>nome_da_mae</th>
-        <th>observacao</th>
-        <th>telefone</th>
-        <th>whatsapp</th>
-        <th>email</th>
-        <th>cidade</th>
-        <th>bairro</th>
-        <th>logradouro</th>
-        <th>numero_residencia</th>
-        <th>aprovacao</th>
+        {{-- <th>Digitado por</th>
         <th>dose</th>
         <th>chegada</th>
         <th>saida</th>
         <th>numero_lote</th>
         <th>fabricante</th>
-        <th>posto_vacinacao_id</th>
-        <th>etapa_id/Texto exibido na home</th>
         <th>campo selecionado</th>
         <th>Informações(acamado)</th>
         <th>criado</th>
         <th>atualizado</th>
-        <th>apagado</th>
+        <th>apagado</th> --}}
 
 
     </tr>
@@ -41,29 +40,7 @@ use App\Models\LotePostoVacinacao;
     <tbody>
     @foreach($candidatos as $candidato)
         <tr>
-            <td>{{ $loop->iteration }}</td>
-            <td>{{ $candidato->nome_completo }}</td>
-            <td>{{ date('d/m/Y', strtotime($candidato->data_de_nascimento)) }}</td>
-            <td>{{ $candidato->idade }}</td>
-            <td>{{ $candidato->cpf }}</td>
-            @php
-                $pieces = explode(" ", $candidato->numero_cartao_sus);
-            @endphp
-            <td>{{ implode(".", $pieces)  }}</td>
-            <td>{{ $candidato->sexo }}</td>
-            <td>{{ $candidato->nome_da_mae }}</td>
-            <td>{{ $candidato->observacao }}</td>
-            <td>{{ $candidato->telefone }}</td>
-            <td>{{ $candidato->whatsapp }}</td>
-            <td>{{ $candidato->email }}</td>
-            <td>{{ $candidato->cidade }}</td>
-            <td>{{ $candidato->bairro }}</td>
-            <td>{{ $candidato->logradouro }}</td>
-            <td>{{ $candidato->numero_residencia }}</td>
-            <td>{{ $candidato->aprovacao }}</td>
-            <td>{{ $candidato->dose }}</td>
-            <td>{{ date('d/m/Y', strtotime($candidato->chegada)) }}</td>
-            <td>{{ date('d/m/Y', strtotime($candidato->saida)) }}</td>
+            {{-- <td>{{ $loop->iteration }}</td> --}}
             @php
                 $lote = LotePostoVacinacao::find($candidato->lote_id);
                 if($lote != null){
@@ -71,26 +48,41 @@ use App\Models\LotePostoVacinacao;
                 }
             @endphp
             @if ($candidato->lote_id)
-                <td>{{  $lote ? $lote->numero_lote : "Erro"  }} </td>
-                <td>{{  $lote ? $lote->fabricante : "Erro"  }}</td>
-                <td>{{ $candidato->posto->nome ?? "posto" }}</td>
-                {{-- @if ($candidato->etapa->tipo == $tipos[0])
-                    <td> {{ 'De '.$candidato->etapa->inicio_intervalo." às ".$candidato->etapa->fim_intervalo}}</td>
-                @elseif($candidato->etapa->tipo == $tipos[1] || $candidato->etapa->tipo == $tipos[2])
-                    <td> {{$candidato->etapa->texto}} </td>
-                @endif
-                --}}
+                
+                
                 @if ($candidato->etapa->tipo == $tipos[0] || $candidato->etapa->tipo == $tipos[1] )
                     <td>{{$candidato->etapa->texto}}</td>
-                    <td> </td>
+                    <td>{{ $candidato->posto->nome ?? "posto" }}</td>
+                    <td> {{$candidato->etapa->texto}}</td>
                 @elseif($candidato->etapa->tipo == $tipos[2])
                     <td>{{$candidato->etapa->texto}}</td>
+                    <td>{{ $candidato->posto->nome ?? "posto" }}</td>
                     <td>
                     @if(App\Models\OpcoesEtapa::find($candidato->etapa_resultado) != null)
                         {{App\Models\OpcoesEtapa::find($candidato->etapa_resultado)->opcao}}
+                    @else
+                        {{$candidato->etapa->texto}}
                     @endif
                     </td>
                 @endif
+                <td>{{ $candidato->cpf }}</td>
+                @php
+                    $sus = explode(" ", $candidato->numero_cartao_sus);
+                @endphp
+                <td>{{ implode(".", $sus)  }}</td>
+                <td>{{ $candidato->nome_completo }}</td>
+                <td>{{ date('d/m/Y', strtotime($candidato->data_de_nascimento)) }}</td>
+                <td>{{ $candidato->sexo }}</td>
+                <td>{{ $candidato->nome_da_mae }}</td>
+                <td>{{ date('d/m/Y \à\s H:i\h', strtotime($candidato->updated_at)) }}</td>
+                <td>{{  $lote ? $lote->fabricante : "Erro"  }}</td>
+                <td>{{ $candidato->dose }}</td>
+                <td>{{  $lote ? $lote->numero_lote : "Erro"  }} </td>
+                <td>{{  $lote ? $lote->fabricante : "Erro"  }}</td>
+                <td>{{  "Nome do vacinador"  }}</td>
+                <td>{{  "Digitado por"  }}</td>
+                <td>{{ $candidato->idade }}</td>
+                <td>{{ $candidato->observacao }}</td>
                 <td>
                     @if ($candidato->outrasInfo != null && count($candidato->outrasInfo) > 0)
                         @foreach ($candidato->etapa->outrasInfo as $outraInfo)
@@ -108,9 +100,21 @@ use App\Models\LotePostoVacinacao;
                 <td>{{  "Erro"  }} </td>
                 <td>{{  "Erro"  }} </td>
             @endif
+
+            {{-- <td>{{ $candidato->telefone }}</td>
+            <td>{{ $candidato->whatsapp }}</td>
+            <td>{{ $candidato->email }}</td>
+            <td>{{ $candidato->cidade }}</td>
+            <td>{{ $candidato->bairro }}</td>
+            <td>{{ $candidato->logradouro }}</td>
+            <td>{{ $candidato->numero_residencia }}</td>
+            <td>{{ $candidato->aprovacao }}</td>
+            <td>{{ date('d/m/Y', strtotime($candidato->chegada)) }}</td>
+            <td>{{ date('d/m/Y', strtotime($candidato->saida)) }}</td>
+            
             <td>{{ $candidato->created_at }}</td>
             <td>{{ $candidato->updated_at }}</td>
-            <td>{{ $candidato->deleted_at }}</td>
+            <td>{{ $candidato->deleted_at }}</td> --}}
 
         </tr>
     @endforeach
